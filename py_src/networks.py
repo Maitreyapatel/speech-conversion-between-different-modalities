64d6fce9b6ab352c13f5b8cb274eb0db66d2cdcd
Maitreya <patel.maitreya57@gmail.com>
2019-12-25 18:55:48 +0530

    generator and discriminators with 4 layers each are added

---
+++ py_src/networks.py
@@ -0,0 +1,72 @@
+'''
+Here, the generators and discriminators are pre-defined as per the configuration used in research paper.
+'''
+import numpy as np
+
+import torch.nn as nn
+import torch.autograd as autograd
+import torch.nn.functional as F
+
+
+# Generator (Converts Whisper-to-Speech) consists of DNN
+class dnn_generator(nn.Module):
+    
+    # Weight Initialization [we initialize weights here]
+    def weight_init(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.xavier_uniform_(self.fc3.weight)
+        nn.init.xavier_uniform_(self.out.weight)
+
+        nn.init.xavier_uniform_(self.fc1.bias)
+        nn.init.xavier_uniform_(self.fc2.bias)
+        nn.init.xavier_uniform_(self.fc3.bias)
+        nn.init.xavier_uniform_(self.out.bias)
+
+    def __init__(self, G_in, G_out, w1, w2, w3):
+        super(dnn_generator, self).__init__()
+        
+        self.fc1= nn.Linear(G_in, w1)
+        self.fc2= nn.Linear(w1, w2)
+        self.fc3= nn.Linear(w2, w3)
+        self.out= nn.Linear(w3, G_out)
+
+        #self.weight_init()
+    
+    # Deep neural network [you are passing data layer-to-layer]    
+    def forward(self, x):
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.out(x)
+        return x
+        
+
+
+# Discriminator (Dicriminate between Whispered speeches) also consists of DNN
+class dnn_discriminator(nn.Module):
+
+    def weight_init(self):
+        nn.init.xavier_uniform_(self.fc1.weight)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        nn.init.xavier_uniform_(self.fc3.weight)
+        nn.init.xavier_uniform_(self.out.weight)
+    
+    def __init__(self, D_in, D_out, w1, w2, w3):
+        super(dnn_discriminator, self).__init__()
+        
+        self.fc1= nn.Linear(D_in, w1)
+        self.fc2= nn.Linear(w1, w2)
+        self.fc3= nn.Linear(w2, w3)
+        self.out= nn.Linear(w3, D_out)
+        
+        #self.weight_init()
+        
+    def forward(self, y):
+        
+        y = F.relu(self.fc1(y))
+        y = F.relu(self.fc2(y))
+        y = F.relu(self.fc3(y))
+        y = F.sigmoid(self.out(y))
+        return y
