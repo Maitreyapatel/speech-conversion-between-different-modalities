539a65ab1702777fad739924d3509d3545751dc2
Maitreya <patel.maitreya57@gmail.com>
2019-12-29 00:30:35 +0530

    Feature extraction scripts are tested and sample wav files are added

--- scripts/concatenation_VCC2018.m
+++ scripts/concatenation_VCC2018.m
@@ -5,8 +5,8 @@ clear all;
 close all;
 
 
-filelist=dir(['../dataset/features/US_102/Normal/*.mcc']);
-filelist1=dir(['../dataset/features/US_102/Whisper/*.mcc']);
+filelist=dir(['../dataset/features/US_102/Normal/mcc/*.mcc']);
+filelist1=dir(['../dataset/features/US_102/Whisper/mcc/*.mcc']);
 
 dim=40;result=[];mpsrc=[];mptgt=[];t_scores=[];l=[];lz=[];wr=[];mpsrc1=[];mptgt1=[];Z1=[];Z=[];Z2=[];
 x=[];y=[];X=[];Y=[];path=[];
@@ -15,11 +15,11 @@ for index=1:length(filelist)
 
     fprintf('Processing %s\n',filelist(index).name);
 
-    fid=fopen(['../dataset/features/US_102/Normal/',filelist1(index).name]);
+    fid=fopen(['../dataset/features/US_102/Normal/mcc/',filelist(index).name]);
     x=fread(fid,Inf,'float');
     x=reshape(x,dim,length(x)/dim);
 
-    fid1=fopen(['../dataset/features/US_102/Whisper/',filelist1(index).name]);
+    fid1=fopen(['../dataset/features/US_102/Whisper/mcc/',filelist1(index).name]);
     y=fread(fid1,Inf,'float');
     y=reshape(y,dim,length(y)/dim);
 
@@ -34,9 +34,9 @@ for index=1:length(filelist)
 end
 
 Z=[X];
-save(['./dataset/features/US_102/Normal/Z.mat'],'Z'); 
+save(['../dataset/features/US_102/Normal/Z.mat'],'Z'); 
 Z=[Y];
-save(['./dataset/features/US_102/Whisper/Z.mat'],'Z');
+save(['../dataset/features/US_102/Whisper/Z.mat'],'Z');
 
 
 
--- scripts/feature_Extraction.sh
+++ scripts/feature_Extraction.sh
@@ -4,7 +4,7 @@ fname=`basename $entry1 .wav`
 echo $fname 
 ./ahocoder16_64 "../dataset/data/US_102/Normal/$fname.wav" "../dataset/features/US_102/Normal/f0/$fname.f0" "../dataset/features/US_102/Normal/mcc/$fname.mcc" "../dataset/features/US_102/Normal/fv/$fname.fv"
 done
-exit
+
 
 #!/bin/sh
 for entry1 in `ls ../dataset/data/US_102/Whisper/*.wav`;do
