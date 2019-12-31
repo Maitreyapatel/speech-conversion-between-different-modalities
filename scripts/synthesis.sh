58c339d89baa6af61df899931bc7f217b1bc74ca
Maitreya <patel.maitreya57@gmail.com>
2019-12-31 19:21:58 +0530

    synthesis script checked

--- scripts/synthesis.sh
+++ scripts/synthesis.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-for entry in `ls ../Results/mcc/*.mcc`;do
+for entry in `ls ../results/mcc/*.mcc`;do
 fname=`basename $entry .mcc` 
 echo $fname
-x2x +af < $entry > ../Results/tmp/$fname.mcc
-x2x +af < ../Results/converted_f0/$fname.f0 > ../Results/tmp/$fname.f0
-./ahodecoder16_64 ../Results/tmp/$fname.f0 ../Results/tmp/$fname.mcc ../Results/converted_wav/$fname.wav
-rm -r ../Results/tmp/*
+x2x +af < $entry > ../results/tmp/$fname.mcc
+x2x +af < ../results/converted_f0/$fname.f0 > ../results/tmp/$fname.f0
+./ahodecoder16_64 ../results/tmp/$fname.f0 ../results/tmp/$fname.mcc ../results/converted_wav/$fname.wav
+rm -r ../results/tmp/*
 done
 exit
